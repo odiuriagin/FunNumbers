07f4fbf53864867d4cf20a84f0b681b26f2fd6cf
Oleksandr <odiuriagin@gmail.com>
2019-01-04 20:47:17 -0500

    Complete header

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { NumbersService } from '../numbers.service';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  dateFact: Object;
+
+  constructor(private numbers: NumbersService) { }
+
+  ngOnInit() {
+    this.numbers.getDateFact().subscribe(
+      fact => this.dateFact = fact
+    )
+  }
+
+}
---
+++ src/app/numbers.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NumbersService {
+
+  constructor(private http: HttpClient) { }
+
+  getDateFact() {
+    const today = new Date();
+    const month = today.getMonth() + 1;
+    const date = today.getDay();
+    return this.http.get(`http://numbersapi.com/${month}/${date}/date?json`);
+  }
+
+  getNumberFact() {
+
+  }
+
+}
